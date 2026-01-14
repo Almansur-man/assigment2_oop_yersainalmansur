143ed463cc39020c17806aeff2f9393980f78537
Almansur-man <mansarinoersaino@gmail.com>
2026-01-14 18:00:36 +0500

    Vehicle.java

---
+++ src/Vehicle.java
@@ -0,0 +1,33 @@
+public abstract class Vehicle {
+
+    protected String brand;
+    protected int year;
+    protected Driver driver;
+
+    public Vehicle(String brand, int year) {
+        this.brand = brand;
+        this.year = year;
+    }
+
+    public abstract void startEngine();
+    public abstract void stopEngine();
+
+    public void displayInfo() {
+        System.out.println("Vehicle brand: " + brand + ", year: " + year);
+    }
+
+    public void setDriver(Driver driver) {
+        this.driver = driver;
+    }
+
+    public void displayDriver() {
+        if (driver != null) {
+            driver.displayDriverInfo();
+        } else {
+            System.out.println("No driver assigned");
+        }
+    }
+}
+
+
+  
